00d7446a5f94c2010fb940bc35ced435856906db
방세현 <tpwnstpgus1@naver.com>
2026-04-22 13:18:23 +0900

    [Chore] .

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,6 +44,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ko">
       <body>{children}</body>
+      <Analytics />
     </html>
   );
 }
